95055f9fa645c279c5721ca9924b3e9ea5bd1251
Antoine-dev <aleduc@student.42.fr>
2024-12-04 00:36:14 +0100

    Day3 Part 1 Done

--- Day03/Day03.cs
+++ Day03/Day03.cs
@@ -1,7 +1,25 @@
-﻿class Program
+﻿using System.Text.RegularExpressions;
+
+class Program
 {
     public static void Main()
     {
+        int r = 0;
 
+        string[] input = File.ReadAllLines("input.txt");
+
+        string pattern = @"(mul[(][0-9]{1,3}[,][0-9]{1,3}[)])";
+        foreach (string line in input)
+        {
+            MatchCollection ws = Regex.Matches(line, pattern, RegexOptions.Multiline);
+            foreach (Match w in ws)
+            {
+                string p = @"([0-9]{1,3})";
+                MatchCollection ms = Regex.Matches(w.Value, p);
+
+                r += (int.Parse(ms[0].Value) * int.Parse(ms[1].Value));
+            }
+        }
+        Console.WriteLine($"r -> {r}");
     }
 }
