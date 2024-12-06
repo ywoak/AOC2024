6ea6815a93a3799bcaf997e9e532cf0255a99ac2
Antoine-dev <aleduc@student.42.fr>
2024-12-06 01:10:16 +0100

    Observing 6 directions in the array

--- Day04/day4.py
+++ Day04/day4.py
@@ -1,3 +1,4 @@
+import numpy as np
 import requests
 import os
 
@@ -13,9 +14,32 @@ r = requests.get(url, cookies=cookies)
 test = "SBBSBBS\nBABABAB\nBBMMMBB\nSAMXMAS\nBBMMMBB\nBABABAB\nSBBSBBS\n"
 
 test2 = "..X...\n.SAMX.\n.A..A.\nXMAS.S\n.X...."
-print(test2)
+print(f'Our input as a string -> \n{test2}')
 
-a = [[c for c in s] for s in test2.strip().split('\n')]
+# Make into list[list[char]]
+llc = [[c for c in s] for s in test2.strip().split('\n')]
 
-for i in a:
-    print(i)
+print(f'\nOur input as a list[list[char]] -> ')
+for lc in llc:
+    print(lc)
+
+# Observe horizontal
+print('\nEach horizontal | reverse horizontal line ->')
+for h in llc:
+    h_std = "".join(h)
+    h_rev = h_std[::-1]
+    print(f'{h_std} | {h_rev}')
+
+# Observe vertical
+print('\nEach vertical | reverse vertical line ->')
+for v in zip(*llc):
+    v_std = "".join(v)
+    v_rev = v_std[::-1]
+    print(f'{v_std} | {v_rev}')
+
+# Observe diagonals
+matrix = np.array(llc)
+bl_tr = [matrix[::-1,:].diagonal(i) for i in range(-matrix.shape[0]+1,matrix.shape[1])]
+tl_br = [matrix.diagonal(i) for i in range(matrix.shape[1]-1,-matrix.shape[0],-1)]
+print(f'BL_TR diagonals and their reverses -> {[n.tolist() for n in bl_tr]}\n')
+print(f'TL_BR diagonals and their reverses -> {[n.tolist() for n in tl_br]}')
