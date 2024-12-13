86bb63e0dada7250b9d1382ea9483fbecfef081e
Antoine-dev <aleduc@student.42.fr>
2024-12-13 22:26:28 +0100

    Callable types, passing function ref to avoid duplicate

--- Day07/bridge_repair.py
+++ Day07/bridge_repair.py
@@ -1,4 +1,6 @@
 # Semantics
+from collections.abc import Callable
+
 type Operand = int
 type Operands = list[Operand]
 type Result = int
@@ -6,6 +8,7 @@ type Result = int
 # Types
 type Equation = tuple[Result, Operands]
 type Equations = list[Equation]
+type Verif_function = Callable[[int, int, Operands, int], int]
 
 def load_equations() -> Equations:
     '''
@@ -18,14 +21,14 @@ def load_equations() -> Equations:
     with open('input.txt') as f:
         input = f.read()
 
-    #test = "190: 10 19\n3267: 81 40 27\n83: 17 5\n156: 15 6\n7290: 6 8 6 15\n161011: 16 10 13\n192: 17 8 14\n21037: 9 7 18 13\n292: 11 6 16 20"
+    test = "190: 10 19\n3267: 81 40 27\n83: 17 5\n156: 15 6\n7290: 6 8 6 15\n161011: 16 10 13\n192: 17 8 14\n21037: 9 7 18 13\n292: 11 6 16 20"
 
     equations: Equations = [
         (
             int(res_s),
             [int(operand) for operand in op_s.strip().split(' ')]
         )
-        for line in input.strip().split('\n')
+        for line in test.strip().split('\n')
         for res_s, op_s in [line.split(':')]
     ]
 
@@ -81,31 +84,20 @@ def is_true_third_operator(current: int, target: int, operands: Operands, index:
             is_true(current + operands[index], target, operands, index + 1) or \
             is_true(concat(current, operands[index]), target, operands, index + 1)
 
-def sum_true_equations(equations: Equations) -> int:
+def sum_true_equations(equations: Equations, equation_verification: Verif_function) -> int:
     '''
     Check if each equation is possible and sum there results
     '''
     sum: int = 0
     for results, operands in equations:
-        if is_true(operands[0], results, operands, 1):
-            sum += results
-    return sum
-
-def sum_true_equations_third_operator(equations: Equations) -> int:
-    '''
-    Check if each equation is possible and sum there results
-    We use this one for part 2
-    '''
-    sum: int = 0
-    for results, operands in equations:
-        if is_true_third_operator(operands[0], results, operands, 1):
+        if equation_verification(operands[0], results, operands, 1):
             sum += results
     return sum
 
 def main() -> None:
     equations: Equations = load_equations()
-    print(f"Part1: {sum_true_equations(equations)}")
-    print(f"Part2: {sum_true_equations_third_operator(equations)}")
+    print(f"Part1: {sum_true_equations(equations, is_true)}")
+    print(f"Part1: {sum_true_equations(equations, is_true_third_operator)}")
 
 if __name__ == '__main__':
     main()
