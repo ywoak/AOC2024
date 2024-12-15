eaa66c792d69ca6548ae3532dba9cccd27e4b06f
Antoine-dev <aleduc@student.42.fr>
2024-12-15 22:52:02 +0100

    Save every antennas position

--- Day08/resonant_collinearity.py
+++ Day08/resonant_collinearity.py
@@ -1,3 +1,9 @@
+from collections import defaultdict
+
+type Pos = tuple[int, int]
+type Frequency = str
+type Antennas = defaultdict[Frequency, list[Pos]]
+
 type Map = list[list[str]]
 
 def load_map() -> Map:
@@ -10,9 +16,30 @@ def load_map() -> Map:
             map.append(list(line))
     return map
 
+def is_alpha(char: str) -> bool:
+    return (('a' <= char <= 'z') or ('A' <= char <= 'Z') or ('0' <= char <= '9'))
+
+def save_frequency_positions(map: Map) -> Antennas:
+    antennas: Antennas = defaultdict(list)
+    for row, R in enumerate(map):
+        for col, elem in enumerate(R):
+            if (is_alpha(elem)):
+                antennas[elem].append((row, col))
+
+    return antennas
+
+def find_antinodes(map: Map, antennas: Antennas) -> int:
+    antinode_number: int = 0
+
+    return antinode_number
+
 def main() -> None:
     map: Map = load_map()
-    for r in map: print(r)
+    antennas: Antennas = save_frequency_positions(map)
+    for key, value in antennas.items():
+        print(f"Frequency : {key}, Positions : {value}")
+    antinode_number = find_antinodes(map, antennas)
+    print(f"Part 1: {antinode_number}")
 
 if __name__ == '__main__':
     main()
