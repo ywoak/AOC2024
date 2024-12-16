5b39d30fa2c4cc3d53e6dbf4ac6e8032a9113f2b
Antoine-dev <aleduc@student.42.fr>
2024-12-16 04:16:21 +0100

    Day 8 part 1 done

--- Day08/resonant_collinearity.py
+++ Day08/resonant_collinearity.py
@@ -3,16 +3,17 @@ from collections import defaultdict
 type Pos = tuple[int, int]
 type Frequency = str
 type Antennas = defaultdict[Frequency, list[Pos]]
+type Antinodes = set[Pos]
 
 type Map = list[list[str]]
 
 def load_map() -> Map:
     map: Map = []
 
-    with open('test.txt') as f:
+    with open('input.txt') as f:
         input = f.read()
     for line in input.split('\n'):
-        if bool(line):
+        if line:
             map.append(list(line))
     return map
 
@@ -25,21 +26,54 @@ def save_frequency_positions(map: Map) -> Antennas:
         for col, elem in enumerate(R):
             if (is_alpha(elem)):
                 antennas[elem].append((row, col))
-
+    print(f"Antennas {antennas}")
     return antennas
 
-def find_antinodes(map: Map, antennas: Antennas) -> int:
-    antinode_number: int = 0
+def calculate_antinodes(pos1: Pos, pos2: Pos) -> set[Pos]:
+    x1, y1 = pos1
+    x2, y2 = pos2
+    a1_x = x2 + (x2 - x1)
+    a1_y = y2 + (y2 - y1)
+    a2_x = x1 + (x1 - x2)
+    a2_y = y1 + (y1 - y2)
+    print(f"For positions {pos1} and {pos2}\nThe first antinode is ({a1_x, a1_y})\nThe second is ({a2_x, a2_y})\n")
+    return ({(a1_x, a1_y), (a2_x, a2_y)})
 
-    return antinode_number
+def find_antinodes(antennas: Antennas) -> Antinodes:
+    antinodes: Antinodes = set()
+
+    for positions in antennas.values():
+        print(f"\n{positions}")
+        length = len(positions)
+        for i in range(length):
+            j = i + 1
+            while (j < length):
+                antinodes |= calculate_antinodes(positions[i], positions[j])
+                j += 1
+
+    print(f"antinodes -> {antinodes}")
+    return antinodes
+
+def is_out_of_bound(height: int, width: int, row: int, col: int):
+    return ((0 <= row < height) and (0 <= col < width))
+
+def remove_out_of_bound_antinodes(height: int, width: int, antinodes: Antinodes) -> Antinodes:
+    print(antinodes)
+    antinode_cpy = set(antinodes)
+    print(f"antinode_cpy -> {antinode_cpy}")
+    for position in antinodes:
+        if not (is_out_of_bound(height, width, position[0], position[1])):
+            print(f"the out of bound position is {position}")
+            antinode_cpy.remove(position)
+    print(f"antinode_cpy -> {antinode_cpy}")
+    return antinode_cpy
 
 def main() -> None:
     map: Map = load_map()
     antennas: Antennas = save_frequency_positions(map)
-    for key, value in antennas.items():
-        print(f"Frequency : {key}, Positions : {value}")
-    antinode_number = find_antinodes(map, antennas)
-    print(f"Part 1: {antinode_number}")
+    antinodes: Antinodes = find_antinodes(antennas)
+    antinodes = remove_out_of_bound_antinodes(len(map), len(map[0]), antinodes)
+    print(f"Part 1: {len(antinodes)}")
 
 if __name__ == '__main__':
     main()
