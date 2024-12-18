44068c74c066e080aee145f558930df8a2472e7a
Antoine-dev <aleduc@student.42.fr>
2024-12-18 01:00:02 +0100

    Optimized time, can now run 75 loops, but result is wrong

--- Day11/plutonian_pebbles.py
+++ Day11/plutonian_pebbles.py
@@ -1,71 +1,45 @@
-from tqdm import tqdm
+from collections import defaultdict
 
 type Stone = str
 type Stones = list[Stone]
 
-type MulCache = dict[Stone, int]
-type Cache = dict[Stone, list[Stone]]
+type Count = defaultdict[Stone, int]
 
 def load_map():
     with open('input.txt') as f:
         input = f.read()
-    return [c for c in input.strip().split(' ')]
+    return [c for c in input.split(' ')]
 
-def blink(map: Stones, mul_cache: MulCache) -> Stones:
-    cpy: Stones = []
-    cache: Cache = dict()
+def blink(count: Count) -> Count:
+    cpy: Count = defaultdict(int)
 
-    def f(stone: Stone) -> Stones:
-        tmp: Stones = []
+    for stone, val in count.items():
         length = len(stone)
 
         if (stone == '0'):
-            tmp.append(str(1))
+            tmp: Stones = [str(1)]
         elif (length % 2 == 0):
-            tmp.append(stone[:length//2])
-            tmp.append(str(int(stone[length//2:])))
+            tmp: Stones = [stone[:length//2], str(int(stone[length//2:]))]
         else:
-            if (stone in mul_cache):
-                tmp.append(str(mul_cache[stone]))
-            else:
-                res = int(stone) * 2024
-                mul_cache[stone] = res
-                tmp.append(str(res))
-        cache[stone] = tmp
-        return tmp
+            tmp: Stones = [str(int(stone) * 2024)]
 
-    for stone in map:
-        if (stone in cache):
-            cpy.extend(cache[stone])
-        else:
-            cpy.extend(f(stone))
-
+        for e in tmp:
+            cpy[e] += val
     return cpy
 
-#def blink(map: Map) -> Map:
-#    cpy: Map = []
-#
-#    for stone in map:
-#        length = len(stone)
-#        if (stone == '0'):
-#            cpy.append(str(1))
-#        elif (length % 2 == 0):
-#            cpy.append(stone[:length//2])
-#            #correct_half: str = remove_trailing_zero(stone[length//2:])
-#            cpy.append(str(int(stone[length//2:])))
-#        else:
-#            cpy.append(str(int(stone) * 2024))
-#    return cpy
+def fill_cache(stones: Stones, count_cache: Count) -> Count:
+    for stone in stones:
+        count_cache[stone] += 1
+    return count_cache
 
 def main():
-    mul_cache :MulCache = dict()
-    map: Stones = load_map()
-    #print(f"Initial map -> {map}")
+    stones: Stones = load_map()
+    count_cache: Count = defaultdict(int)
+    count_cache = fill_cache(stones, count_cache)
     for _ in range(25):
-        tqdm()
-        map = blink(map, mul_cache)
-        #print(f"\nmap After blink-> {map}")
-    print(f"Part 1: {len(map)}")
+        count_cache = blink(count_cache)
+
+    print(f"Part 1: {sum(count_cache.values())}")
 
 if __name__ == "__main__":
     main()
