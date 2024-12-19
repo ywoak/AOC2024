ec51742982ae9c92ab214cd571c0984e821c5d37
Antoine-dev <aleduc@student.42.fr>
2024-12-20 00:25:34 +0100

    Some common input parsing types

--- Utils/load_input.py
+++ Utils/load_input.py
@@ -1,7 +1,28 @@
 import sys
 
+# Charge l'entrée depuis stdin
+input = sys.stdin.read()
+
 def load_input() -> str:
     """
-    Charge l'entrée depuis stdin et retourne une chaîne de caractères.
+    Load stdin
     """
-    return sys.stdin.read()
+    return input
+
+def load_lines() -> list[str]:
+    """
+    Load a list of line from stdin
+    """
+    return [line for line in input.strip().split('\n')]
+
+def load_int() -> list[list[int]]:
+    """
+    Load a list of list of int for every line for every elem separated by whitespace
+    """
+    return [[int(elem)] for line in input.strip().split('\n') for elem in line.split(' ')]
+
+def load_str() -> list[list[str]]:
+    """
+    Load a list of list of char for every char in every line
+    """
+    return [[char] for line in input.strip().split('\n') for char in line]
