376aaf2fbe5ee5db2a59fe7847e01a9934644d2f
Antoine-dev <aleduc@student.42.fr>
2024-12-20 08:46:23 +0100

    Day 13 Part 1 Done

--- Day13/claw_contraption.py
+++ Day13/claw_contraption.py
@@ -1,8 +1,34 @@
 from load_input import load_input
+from custom_types import Positions
+
 import re
 
+def find_goal_combination(machine) -> int:
+    (ax, ay), (bx, by), (gx, gy) = machine
+    #print(f"\n{ax, ay, bx, by, gx, gy}")
+
+    gx += 10000000000000
+    gy += 10000000000000
+
+    #print(f"\n{ax, ay, bx, by, gx, gy}")
+    upper_range: int = gx // ax
+    #print(f"Upper range -> {upper_range}")
+    i = upper_range
+    while i > 0:
+        #print(i)
+        anum = gx - ax * i
+        bnum: float = anum / bx
+        ibnum: int = int(bnum)
+        if bnum == ibnum: # and bnum <= 100: for part 1
+            if (gy == (ay * i) + (by * ibnum)): # and i <= 100 and int(bnum) <= 100): for part 1
+                #print(f"We have found the correct number of A and B -> {i, int(bnum)}")
+                return 3 * i + int(bnum)
+        i -= 1
+    #print('Done 1 machine')
+    return 0
+
 def main():
-    machines = [
+    machines: list[Positions] = [
         [
             tuple(int(coord) for coord in re.findall(r'\d+', instruction))
             for instruction in machine.split('\n')
@@ -10,9 +36,11 @@ def main():
         for machine in load_input().split("\n\n")
     ]
 
-    print(machines)
+    #print(machines)
+    sum = 0
     for machine in machines:
-        print(f"\n{machine}")
+        sum += find_goal_combination(machine)
+    print(f"part 1: {sum}")
 
 if __name__ == "__main__":
     main()
