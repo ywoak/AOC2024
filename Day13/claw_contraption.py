7c38f0ccded0519f7cad128a67e22e91d1a56aee
Antoine-dev <aleduc@student.42.fr>
2024-12-20 08:56:30 +0100

    Revert to part 1 code for this file only

--- Day13/claw_contraption.py
+++ Day13/claw_contraption.py
@@ -5,26 +5,17 @@ import re
 
 def find_goal_combination(machine) -> int:
     (ax, ay), (bx, by), (gx, gy) = machine
-    #print(f"\n{ax, ay, bx, by, gx, gy}")
 
-    gx += 10000000000000
-    gy += 10000000000000
-
-    #print(f"\n{ax, ay, bx, by, gx, gy}")
     upper_range: int = gx // ax
-    #print(f"Upper range -> {upper_range}")
     i = upper_range
     while i > 0:
-        #print(i)
         anum = gx - ax * i
         bnum: float = anum / bx
         ibnum: int = int(bnum)
-        if bnum == ibnum: # and bnum <= 100: for part 1
-            if (gy == (ay * i) + (by * ibnum)): # and i <= 100 and int(bnum) <= 100): for part 1
-                #print(f"We have found the correct number of A and B -> {i, int(bnum)}")
+        if bnum == ibnum and bnum <= 100:
+            if (gy == (ay * i) + (by * ibnum) and i <= 100 and int(bnum) <= 100):
                 return 3 * i + int(bnum)
         i -= 1
-    #print('Done 1 machine')
     return 0
 
 def main():
@@ -36,7 +27,6 @@ def main():
         for machine in load_input().split("\n\n")
     ]
 
-    #print(machines)
     sum = 0
     for machine in machines:
         sum += find_goal_combination(machine)
