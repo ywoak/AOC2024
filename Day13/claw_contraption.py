4d177a7f026a3379f24cc1ab65d4c58c42b60eb7
Antoine-dev <aleduc@student.42.fr>
2024-12-21 01:48:13 +0100

    Day 13 Part 2 Done with equation systems

--- Day13/claw_contraption.py
+++ Day13/claw_contraption.py
@@ -1,9 +1,9 @@
 from load_input import load_input
 from custom_types import Positions
-
+from sympy import Rational
 import re
 
-def find_goal_combination(machine) -> int:
+def find_goal_combination(machine: Positions) -> int:
     (ax, ay), (bx, by), (gx, gy) = machine
 
     upper_range: int = gx // ax
@@ -18,6 +18,30 @@ def find_goal_combination(machine) -> int:
         i -= 1
     return 0
 
+def solve_equations(machine: "Positions") -> int:
+    (nxA, nyA), (nxB, nyB), (gx, gy) = machine
+
+    gx += 10000000000000
+    gy += 10000000000000
+
+    # Use rational to aovid precision error
+    nxA = Rational(nxA)
+    nyA = Rational(nyA)
+    nxB = Rational(nxB)
+    nyB = Rational(nyB)
+    gx = Rational(gx)
+    gy = Rational(gy)
+
+    # Equation system of 2 with 2 unknown
+    b = (gy - ((gx * nyA) / nxA)) / (nyB - ((nxB * nyA) / nxA))
+    a = (gx - (b * nxB)) / nxA
+
+    # Check for int, if 40.0 then we have the correct result, if 40.2 then no
+    res = 0
+    if a == int(a) and b == int(b):
+        res = int(a) * 3 + int(b)
+    return res
+
 def main():
     machines: list[Positions] = [
         [
@@ -27,10 +51,13 @@ def main():
         for machine in load_input().split("\n\n")
     ]
 
-    sum = 0
+    part1 = 0
+    part2 = 0
     for machine in machines:
-        sum += find_goal_combination(machine)
-    print(f"part 1: {sum}")
+        part1 += find_goal_combination(machine)
+        part2 += solve_equations(machine)
+    print(f"part 1: {part1}")
+    print(f"part 2: {part2}")
 
 if __name__ == "__main__":
     main()
