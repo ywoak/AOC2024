97e85a9b2b58c1e58c12f1f020c737acf19dafc4
Antoine-dev <aleduc@student.42.fr>
2024-12-23 23:38:37 +0100

    Clean reading

--- Day15/warehouse_woes.py
+++ Day15/warehouse_woes.py
@@ -1,6 +1,15 @@
-map, commands = open(0).read().strip().split('\n\n')
-commands = commands.replace("\n", "")
-print(map, end="\n\n")
-print(commands)
+def parse_input() -> tuple[list[list[str]], str]:
+    input: list[str] = open(0).read().strip().split('\n\n')
+    map: list[list[str]] = [[char for char in line] for line in input[0].split('\n')]
+    commands: str = input[1].replace("\n", "")
 
+    return map, commands
 
+def main():
+    map, commands = parse_input()
+
+    print(map, end="\n\n")
+    print(commands)
+
+if __name__ == "__main__":
+    main()
