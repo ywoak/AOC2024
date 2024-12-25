e58f85649b4d9b69996f87c1fa7c595281b43d72
Antoine-dev <aleduc@student.42.fr>
2024-12-25 07:22:51 +0100

    Implementing a ScoreDict to keep the winning path, Bugfix: reset orientation east for each new past when calculating best path

--- Day16/reindeer_maze.py
+++ Day16/reindeer_maze.py
@@ -8,6 +8,8 @@ type Paths = list[Path]
 
 type Map = list[list[str]]
 
+type ScoreDict = dict[int, int]
+
 def load_map() -> Map:
     return [
         [char for char in line]
@@ -49,32 +51,82 @@ def color_map_with_path(map: Map, path: Positions):
         if map[x][y] not in 'SE':
             map[x][y] = '|'
 
-def find_lowest_score(paths) -> int:
-    lowest_score: int = 0
-    #dir = 
-    #for path in paths:
+def find_lowest_score(paths: Paths) -> ScoreDict:
+    """
+    Iterate each path to verify which cost the lowest score
+    Turning cost 1000, Moving forward once cost 1
+    We start by looking east
+    We never do a 180 turn
+    """
+    directions: Positions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    scores: ScoreDict = dict()
 
-    return lowest_score
+    for P, path in enumerate(paths):
+        orientation = 1 # Start East
+        #print(f"\nFor path {j} ->")
+        score: int = 0
+        for i in range(len(path) - 1):
+            x, y = path[i]
+            nx, ny = path[i + 1]
+            #print(f"We are on {x, y}, we are taking into account {nx, ny}")
+            dx, dy = directions[orientation]
+
+            if not ((nx == x + dx) and (ny == y + dy)):
+                score += 1000
+                orientation = directions.index((nx - x, ny - y))
+                #print(f"For path {path}\nNew orientation is {orientation} from node ({x, y}) to node ({nx, ny})")
+            #print(f"We move forward")
+            score += 1
+
+        #print(f"Final score for path is {score}")
+        scores[P] = score
+
+    return scores
 
 # Expected result => 7036 & 11048
 # Test 4 expected result, 4 different path, got 3
 def main():
     map: Map = load_map()
     H, W = len(map), len(map[0])
-    for r in map: print(r)
+    #for r in map: print(r)
 
     start, end = find_start_and_end(map, H, W)
-    print(f"Start is {start}, end is {end}")
+    #print(f"Start is {start}, end is {end}")
 
     paths = solve_maze(map, start, end)
-    for i, path in enumerate(paths, 1):
-        print(f"Chemin {i}: {path}")
-        cpy_map = [[char for char in line] for line in map]
-        color_map_with_path(cpy_map, path)
-        for r in cpy_map: print(r)
+    #for i, path in enumerate(paths, 1):
+        #print(f"Chemin {i}: {path}")
 
-    score: int = find_lowest_score(paths)
-    print(f"Part 1: {score}")
+    scores: ScoreDict = find_lowest_score(paths)
+
+
+#    directions: Positions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+#    orientation = 1 # Start East
+
+    min_key = min(scores, key=lambda k: scores[k])
+
+#    cpy_map = [[char for char in line] for line in map]
+#    color_map_with_path(cpy_map, paths[min_key])
+#    for r in cpy_map: print(r)
+#
+#    score: int = 0
+#    for i in range(len(paths[min_key]) - 1):
+#        x, y = paths[min_key][i]
+#        nx, ny = paths[min_key][i + 1]
+#        #print(f"\nWe are on {x, y}, we are looking at {nx, ny}")
+#        dx, dy = directions[orientation]
+#
+#        if not ((nx == x + dx) and (ny == y + dy)):
+#            score += 1000
+#            orientation = directions.index((nx - x, ny - y))
+#            print(f"TURN AT {x, y} -> {orientation}")
+#
+#        #print(f"We move forward")
+#        score += 1
+#
+#    print(f"SCOORUUUUU is {score}")
+
+    print(f"\nPart 1: {scores[min_key]}")
 
 if __name__ == "__main__":
     main()
