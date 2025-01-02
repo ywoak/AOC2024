1bd4c3881a110bbb1cc61edf20be0f3a0ef422a1
Antoine-dev <aleduc@student.42.fr>
2025-01-02 23:44:13 +0100

    Dont achieve weight handling with adjacency list

--- Day16/reindeer_maze.py
+++ Day16/reindeer_maze.py
@@ -38,11 +38,14 @@ def dijkstra(graph: Map, distances: Distances, start: Position, end: Position) -
     print(f"Distance after setting start is {distances}")
     pq: list[tuple[int, Position]] = [(0, start)] # (distance, node)
     score: int = 0
+    cur_dir: int = 1 # Starting west
+    directions: Positions = [(1, 0), (0, 1), (-1, 0), (0, -1)] # Keep 90 degre rotation close to each other, to always have dir +1/-1
 
     # For each position, determine distance
     while pq:
         current_distance, current_node = heapq.heappop(pq)
         x, y = current_node
+
 
         if current_distance > distances[current_node]:
             continue
@@ -52,7 +55,17 @@ def dijkstra(graph: Map, distances: Distances, start: Position, end: Position) -
         # And then grab the weight depending on the direction we're looking
         # Or keep graph(current_node).items() but then modify graph, to have an adjacency list
         # Instead of the current map with only str without weight to destructur
-        for neighbor, weight in graph[x][y]:
+
+        # for the 4 directions, have them 90 degre rotation
+        # Keep a current direction, one starting west
+
+        # for neighbor, weight in graph[x][y]:
+        for dx, dy in (directions[cur_dir+1], directions[cur_dir-1]):
+            nx, ny = x + dx, y + dy
+            # if we're looking 90 degree, so current direction +1 or -1
+            # weight is 1000
+            # Else weight is 1
+
             distance = current_distance + weight
 
             if distance < distances[neighbor]:
