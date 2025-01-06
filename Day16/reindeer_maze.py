e48c387b26c0189771a644f9aeac54daf934c070
Antoine-dev <aleduc@student.42.fr>
2025-01-06 18:02:43 +0100

    Day 16 Part 1 With dijkstra done, clean code and add comments

--- Day16/reindeer_maze.py
+++ Day16/reindeer_maze.py
@@ -34,42 +34,39 @@ def find_start_and_end(map: Map, H: int, W: int) -> tuple[Position, Position, Di
     return start, end, distances
 
 def dijkstra(graph: Map, distances: Distances, start: Position, end: Position) -> int | float:
+    """
+    Keep 4 directions next to each other clockwise, to be able to always look at possible directions with +1/-1
+    Always checking only 3 directions, since we have where we came from, no need to check behind us
+    Starting position is East
+
+    Standard Dijkstra for the rest, getting the weight depending on where we looking, and the next direction to put in the heap
+    The heap is pq, with (distance, node, direction)
+
+    If we are somewhere we've already been but distance was lower, no need to continue this path
+    If we reach the end, end it (not sure if its needed, but it's supposed to be faster)
+
+    Checking for out of bound only done with obstacle #, because it surrounds the map, so we never get out
+
+    This function "can" return float, but it will always return int on a correct maze, inf should never be the answer, I will even assert it
+    """
     distances[start] = 0
-    cur_dir: int = 1 # Starting position is East
-    pq: list[tuple[int, Position, int]] = [(0, start, cur_dir)] # (distance, node, direction)
+    cur_dir: int = 1
+    pq: list[tuple[int, Position, int]] = [(0, start, cur_dir)]
 
-    # Keep 90 degre rotation close to each other for 4 rotations, to always have dir +1/-1
-    directions: Positions = [(1, 0), (0, 1), (-1, 0), (0, -1)] # South, East, North, West
+    directions: Positions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-    # For each position, determine distance
     while pq:
         current_distance, current_node, cur_dir = heapq.heappop(pq)
         x, y = current_node
 
-        print(f"\nIm at position {x, y}")
         if current_distance > distances[current_node]: continue
         if (x, y) == end: break
 
-        # Get neighbor and weight,
-
-        # either do it with x, y destructor to reach all 4 neighbor
-        # And then grab the weight depending on the direction we're looking
-
-        # Or keep graph(current_node).items() but then modify graph, to have an adjacency list
-        # Instead of the current map with only str without weight to destructur
-
-        # for neighbor, weight in graph[x][y]:
         for dx, dy in (directions[cur_dir], directions[(cur_dir + 1) % 4], directions[(cur_dir - 1) % 4]):
             nx, ny = x + dx, y + dy
-            # Check for out of bound
             if graph[nx][ny] == '#': continue
 
-            # if we're looking 90 degree, so current direction +1 or -1
-            # weight is 1000
-            # Else weight is 1
             weight = 1001 if (dx, dy) != directions[cur_dir] else 1
-            print(f"\nIm looking at direction {nx, ny}")
-            print(f"cur_dir is {cur_dir}")
 
             if (dx, dy) == directions[(cur_dir + 1) % 4]:
                 next_dir = (cur_dir + 1) % 4
@@ -79,33 +76,22 @@ def dijkstra(graph: Map, distances: Distances, start: Position, end: Position) -
                 next_dir = cur_dir
 
             distance = current_distance + weight
-            print(f"weight is {weight}, so new distance is {distance}")
-            print(f"Old distance is {distances[(nx, ny)]}")
 
             if distance < distances[(nx, ny)]:
                 distances[(nx, ny)] = distance
                 heapq.heappush(pq, (distance, (nx, ny), next_dir))
 
-    print(f"Distances after dijkstra is:\n")
-    for key, val in distances.items():
-        x, y = key
-        if graph[x][y] != '#':
-            print(f"{key}: {val}")
-
     return distances[end]
 
-# Expected result => 7036 & 11048
-# Test 4 expected result, 4 different path, got 3 -> Solved
+# Expected tests results => 7036 & 11048
 def main():
     map: Map = load_map()
     H, W = len(map), len(map[0])
-    for r in map: print(r)
 
     start, end, distances = find_start_and_end(map, H, W)
-    print(f"Start is {start}, end is {end}\n")
-    print(f"Distances before dijkstra is:\n {distances}\n")
 
     score: int | float = dijkstra(map, distances, start, end)
+    assert type(score) == int
     print(f"Score is {score}")
 
 if __name__ == "__main__":
