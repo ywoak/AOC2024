b22fa334879c280841d1874ef8e44765b08e7fba
Antoine-dev <aleduc@student.42.fr>
2025-01-06 16:40:08 +0100

    Dijkstra v1 finished for current arch, but we have a wrong answer, seems 1000 weight is never taken into account

--- Day16/reindeer_maze.py
+++ Day16/reindeer_maze.py
@@ -33,61 +33,57 @@ def find_start_and_end(map: Map, H: int, W: int) -> tuple[Position, Position, Di
     assert start != (0, 0) and end != (0, 0), "The map is incorrect"
     return start, end, distances
 
-def dijkstra(graph: Map, distances: Distances, start: Position, end: Position) -> int:
+def dijkstra(graph: Map, distances: Distances, start: Position, end: Position) -> int | float:
     distances[start] = 0
-    print(f"Distance after setting start is {distances}")
     pq: list[tuple[int, Position]] = [(0, start)] # (distance, node)
     score: int = 0
-    cur_dir: int = 1 # Starting west
-    directions: Positions = [(1, 0), (0, 1), (-1, 0), (0, -1)] # Keep 90 degre rotation close to each other, to always have dir +1/-1
+
+    # Keep 90 degre rotation close to each other for 4 rotations, to always have dir +1/-1
+    # Keep a current direction, one starting west
+    directions: Positions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    cur_dir: int = 1
 
     # For each position, determine distance
     while pq:
         current_distance, current_node = heapq.heappop(pq)
         x, y = current_node
 
-
-        if current_distance > distances[current_node]:
-            continue
+        if current_distance > distances[current_node]: continue
 
         # Get neighbor and weight,
-        # either do it with x, y destructur to reach all 4 neighbor
+
+        # either do it with x, y destructor to reach all 4 neighbor
         # And then grab the weight depending on the direction we're looking
+
         # Or keep graph(current_node).items() but then modify graph, to have an adjacency list
         # Instead of the current map with only str without weight to destructur
 
-        # for the 4 directions, have them 90 degre rotation
-        # Keep a current direction, one starting west
-
         # for neighbor, weight in graph[x][y]:
-        # 
-        for dx, dy in (directions[cur_dir+1], directions[cur_dir-1]):
+        for dx, dy in (directions[cur_dir + 1], directions[cur_dir - 1], directions[cur_dir]):
             nx, ny = x + dx, y + dy
+            # Check for out of bound
+            if graph[nx][ny] == '#': continue
+
             # if we're looking 90 degree, so current direction +1 or -1
             # weight is 1000
+            weight = 1000
             # Else weight is 1
+            if (dx, dy == directions[cur_dir]):
+                weight = 1
 
             distance = current_distance + weight
 
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                heapq.heappush(pq, (distance, neighbor))
+            if distance < distances[(nx, ny)]:
+                distances[(nx, ny)] = distance
+                heapq.heappush(pq, (distance, (nx, ny)))
 
-        # Perhaps duplicate with only one other direction, because we have a direction im not sure if we need to check the 4 directions
-        # We can never do a 180 degree
-        dx, dy = (directions[cur_dir])
-        nx, ny = x + dx, y + dy
-        # if we're looking 90 degree, so current direction +1 or -1
-        # weight is 1000
-        # Else weight is 1
+    print(f"Distances after dijkstra is:\n")
+    for key, val in distances.items():
+        x, y = key
+        if graph[x][y] != '#':
+            print(f"{key}: {val}")
 
-        distance = current_distance + weight
-
-        if distance < distances[neighbor]:
-            distances[neighbor] = distance
-            heapq.heappush(pq, (distance, neighbor))
-
-    return score
+    return distances[end]
 
 # Expected result => 7036 & 11048
 # Test 4 expected result, 4 different path, got 3 -> Solved
@@ -97,9 +93,11 @@ def main():
     for r in map: print(r)
 
     start, end, distances = find_start_and_end(map, H, W)
-    print(f"Start is {start}, end is {end}\n distance is {distances}")
+    print(f"Start is {start}, end is {end}\n")
+    print(f"Distances before dijkstra is:\n {distances}\n")
 
-    dijkstra(map, distances, start, end)
+    score: int | float = dijkstra(map, distances, start, end)
+    print(f"Score is {score}")
 
 if __name__ == "__main__":
     main()
